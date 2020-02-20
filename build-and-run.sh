ffc74a02589d9e4baa8ac76bbdb474145c2f4574
Petri Sirkkala <sirpete@iki.fi>
2020-02-20 15:09:58 +0200

    Start by running this

---
+++ build-and-run.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+BASEDIR=$(cd $(dirname $0); /bin/pwd)
+
+cd "${BASEDIR}"
+app/gradlew build
+cp app/build/libs/rest-service-0.0.1-SNAPSHOT.jar docker/awesome.jar
+
+cd docker
+docker build . --tag awesome:latest
+docker run --user $(id -u):$(id -g) -p 127.0.0.1:8080:8080 awesome:latest
