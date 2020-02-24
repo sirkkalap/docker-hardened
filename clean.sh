98190313375043ca09154dd697e6d2634a872f46
Petri Sirkkala <petri.sirkkala@solita.fi>
2020-02-24 14:14:29 +0200

    Clean script

---
+++ clean.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+BASEDIR=$(cd $(dirname $0); /bin/pwd)
+set -e
+
+(
+    cd "${BASEDIR}/app"
+    ./gradlew --no-daemon clean
+)
+rm -f docker/awesome.jar
+
+docker stop awesome-service || true
+docker rm awesome-service || true
+docker rmi awesome:latest || true
